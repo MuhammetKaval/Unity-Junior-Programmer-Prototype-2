144c10174ff580314934d0079b219b3cf0f0e04c
MuhammetKaval <muhammetkaval@yahoo.com>
2022-04-11 15:51:53 +0300

    Challenge 2 - Play Fetch Done

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    public int timeBetweenSend = 1;
+    public int timeCounter = 0;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && timeCounter == 0)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            timeCounter = timeBetweenSend;
+
+            StartCoroutine("IncreaseCount");
+        }
+    }
+
+
+    // Countdown for send a dog
+    IEnumerator IncreaseCount()
+    {
+        while(timeCounter > 0)
+        {
+            Debug.Log(timeCounter);
+            yield return new WaitForSeconds(1f);
+            timeCounter--;
+        }
+    }
+}
